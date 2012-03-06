1cacb61000f7e8a184d605b34df9a9091a177142
Philip C. Roth <rothpc@ornl.gov>
2012-03-06 10:03:46 -0500

    Updated OS X config to build 64-bit executables by default.

--- config/conf-osx.sh
+++ config/conf-osx.sh
@@ -2,23 +2,24 @@
 
 # Configure to build OpenCL and CUDA tests.
 
-# By default, we build 32-bit executables on OS X because pre-4.0 versions
-# of CUDA do not provide 64-bit support for all libraries.  
+# By default, we build 64-bit executables on OS X because CUDA 4.0 and later
+# provide 64-bit support for all libraries.  
 #
-# However, if you are using CUDA 4.0 on a Mac with a x86_64 processor, you 
-# can add -m64 to compile flags to build 64-bit executables (assuming you are
-# building with gcc - use whatever flags are necessary for your compiler).  For
-# example:
-#
-# sh ./configure \
-#    CXXFLAGS="-m64" \
-#    CFLAGS="-m64" \
-#    NVCXXFLAGS="-m64" \
-#    --with-opencl --with-cuda
-#
-sh ./configure \
-    CXXFLAGS="-m32" \
-    CFLAGS="-m32" \
-    NVCXXFLAGS="-m32" \
-    --with-opencl --with-cuda 
+# However, if you are using an earlier version of CUDA than 4.0, or
+# are on a Mac without a x86_64 processor, you can change the -m64 flag
+# to be -m32 in the configure script below to build 32-bit executables 
+# (assuming you are building with gcc - use whatever flags are necessary 
+# for your compiler).  For example:
+#sh ./configure \
+#    CXXFLAGS="-m32" \
+#    CFLAGS="-m32" \
+#    NVCXXFLAGS="-m32" \
+#    --with-opencl --with-cuda 
+
+
+ sh ./configure \
+    CXXFLAGS="-m64" \
+    CFLAGS="-m64" \
+    NVCXXFLAGS="-m64" \
+    --with-opencl --with-cuda
 
