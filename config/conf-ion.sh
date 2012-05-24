138b9231ec62057a146eb99538bdcc37e92815eb
Jeremy S. Meredith <js9@ion.ornl.gov>
2012-05-24 14:44:54 -0400

    Need SM/Compute version 3.0 for GTX680.

--- config/conf-ion.sh
+++ config/conf-ion.sh
@@ -4,7 +4,6 @@
 
 PATH="/usr/local/cuda42/cuda/bin:$PATH"
 CPPFLAGS="-I/usr/local/cuda42/cuda/include/" \
-CUDA_CPPFLAGS="-gencode=arch=compute_20,code=sm_20 -DUSE_CLOCK_GETTIME" \
-CUDA_LDFLAGS="-Xlinker -rpath=/usr/local/cuda42/cuda/lib64" \
+CUDA_CPPFLAGS="-DUSE_CLOCK_GETTIME -gencode=arch=compute_20,code=sm_20 -gencode=arch=compute_30,code=sm_30" \
 sh ./configure
 
