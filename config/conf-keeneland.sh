dec1c8df68264073806831ace727450044da7ccd
Philip C. Roth <rothpc@ornl.gov>
2012-05-29 14:05:14 -0400

    Changed to generate code only for GPUs found within the KIDS system.

--- config/conf-keeneland.sh
+++ config/conf-keeneland.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-# do the actual configuration
-# if using TAUcuda:
-#sh ./configure CC=tau_cc.sh CXX=tau_cxx.sh \
-
-# otherwise:
 sh ./configure \
-CPPFLAGS="-I/sw/keeneland/cuda/4.1/linux_binary/include"
+CPPFLAGS="-I/sw/keeneland/cuda/4.1/linux_binary/include" \
+CUDA_CPPFLAGS="-gencode=arch=compute_20,code=sm_20 -gencode=arch=compute_20,code=compute_20"
 
