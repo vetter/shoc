e8372401e48adcf0afa800ed0d9a1ccc887fcbc5
Kyle Spafford <kyle.spafford@gmail.com>
2012-06-05 17:53:06 -0400

    updating newark config

--- config/conf-newark.sh
+++ config/conf-newark.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CPPFLAGS="-I/opt/cuda-4.2/cuda/include/" \
-CUDA_CPPFLAGS="-gencode=arch=compute_20,code=sm_20" \
+CPPFLAGS="-I/opt/cuda/4.2/cuda/include/" \
+CUDA_CPPFLAGS="-gencode=arch=compute_20,code=sm_20 -gencode=arch=compute_30,code=sm_30" \
 sh ./configure
 
