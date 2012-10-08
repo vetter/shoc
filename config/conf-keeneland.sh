7c235a8090ac76029b31639beabef8ad6475223e
Kyle Spafford <kyle.spafford@gmail.com>
2012-10-08 15:16:27 -0400

    updated keeneland config, fixed sort bug

--- config/conf-keeneland.sh
+++ config/conf-keeneland.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 sh ./configure \
-CPPFLAGS="-I/sw/keeneland/cuda/4.2/linux_binary/include" \
+CPPFLAGS="-I/sw/kfs/cuda/4.2/linux_binary/include" \
 CUDA_CPPFLAGS="-gencode=arch=compute_20,code=sm_20 -gencode=arch=compute_20,code=compute_20"
 
