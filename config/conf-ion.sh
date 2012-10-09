b2f8f03324fc7db17a02eb78c02362bbe2a4322e
Jeremy Meredith <jsmeredith@gmail.com>
2012-10-09 17:46:07 -0400

    update to workstation config.

--- config/conf-ion.sh
+++ config/conf-ion.sh
@@ -2,7 +2,8 @@
 # note: "ion" is an Ubuntu 12.04 system with gcc 4.6.x
 
 PATH="/usr/local/cuda42/cuda/bin:$PATH"
+
+./configure \
 CPPFLAGS="-I/usr/local/cuda42/cuda/include/" \
-CUDA_CPPFLAGS="-DUSE_CLOCK_GETTIME -gencode=arch=compute_20,code=sm_20 -gencode=arch=compute_30,code=sm_30" \
-sh ./configure
+CUDA_CPPFLAGS="-DUSE_CLOCK_GETTIME -gencode=arch=compute_11,code=sm_11 -gencode=arch=compute_20,code=sm_20 -gencode=arch=compute_30,code=sm_30"
 
