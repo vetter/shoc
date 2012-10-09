7d5bed870e7e27a0495a89c5d87b73b79dfe2134
Philip C. Roth <rothpc@ornl.gov>
2012-10-09 08:41:14 -0400

    Added configure support and docs for using PGI compiler.

--- config/conf-atlanta.sh
+++ config/conf-atlanta.sh
@@ -2,12 +2,25 @@
 
 OCL_ROOT=/opt/AMDAPP
 
+# A "good" set of optimization flags is compiler dependent.
+# These might be reasonable flags to start from.
+#
+# GNU
+OPTFLAGS="-g -O2"
+
+# Intel
+#OPTFLAGS="-g -fast"
+
+# PGI
+#OPTFLAGS="-g -fastsse"
+
+
 # do the actual configuration
 sh ./configure \
     CPPFLAGS="-I$OCL_ROOT/include" \
-    CXXFLAGS="-g -O2" \
-    CFLAGS="-g -O2" \
-    LDFLAGS="-g -O2 -L$OCL_ROOT/lib/x86_64" \
+    CXXFLAGS="$OPTFLAGS" \
+    CFLAGS="$OPTFLAGS" \
+    LDFLAGS="$OPTFLAGS -L$OCL_ROOT/lib/x86_64" \
     --without-cuda \
     --disable-stability
 
