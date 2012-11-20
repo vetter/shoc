40be16b0058cb7f24afefabbb7b943378fd120b0
Philip C. Roth <rothpc@ornl.gov>
2012-11-20 14:31:42 -0500

    Updated example conf script for CUDA 5.

--- config/conf-newark.sh
+++ config/conf-newark.sh
@@ -17,7 +17,7 @@ OPTFLAGS="-g -O2"
 #export CXX=pgcpp
 #export CC=pgcc
 
-CPPFLAGS="-I/opt/cuda/4.2/cuda/include/" \
+CPPFLAGS="-I/opt/cuda/5.0/cuda/include/" \
 CUDA_CPPFLAGS="-gencode=arch=compute_20,code=sm_20 -gencode=arch=compute_30,code=sm_30" \
 sh ./configure \
     CXXFLAGS="$OPTFLAGS" \
