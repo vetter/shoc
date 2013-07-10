89d7a6b4451653f00b26335391c7ee0711a27d10
Philip C. Roth <rothpc@ornl.gov>
2013-07-10 13:54:10 -0400

    Factored common Matrix2D file support code.

--- src/cuda/level1/stencil2d/Stencil2Dmain.cpp
+++ src/cuda/level1/stencil2d/Stencil2Dmain.cpp
@@ -14,6 +14,7 @@
 #include "InvalidArgValue.h"
 #include "Matrix2D.h"
 #include "Matrix2D.cpp"
+#include "Matrix2DFileSupport.cpp"
 #include "HostStencilFactory.h"
 #include "HostStencil.h"
 #include "CUDAStencilFactory.h"
@@ -50,70 +51,6 @@ void CheckOptions( const OptionParser& opts );
 
 void EnsureStencilInstantiation( void );
 
-template<class T>
-std::string
-GetMatrixFileName( std::string baseName )
-{
-    // nothing to do - this should never be instantiated
-    assert( false );
-    return "";
-}
- 
-template<>
-std::string
-GetMatrixFileName<float>( std::string baseName )
-{
-    return baseName + "-sp.dat";
-}
-
-template<>
-std::string
-GetMatrixFileName<double>( std::string baseName )
-{
-    return baseName + "-dp.dat";
-}
-
-
- template<class T>
-bool
-SaveMatrixToFile( const Matrix2D<T>& m, std::string fileName )
-{
-    bool ok = true;
-
-    std::ofstream ofs( fileName.c_str(), ios::out | ios::binary );
-    if( ofs.is_open() )
-    {
-        ok = m.WriteTo( ofs );
-        ofs.close();
-    }
-    else
-    {
-        std::cerr << "Unable to write matrix to file \'" << fileName << "\'" << std::endl;
-        ok = false;
-    }
-    return ok;
-}
-
-
-template<class T>
-bool
-ReadMatrixFromFile( Matrix2D<T>& m, std::string fileName )
-{
-    bool ok = true;
-
-    std::ifstream ifs( fileName.c_str(), ios::in | ios::binary );
-    if( ifs.is_open() )
-    {
-        ok = m.ReadFrom( ifs );
-        ifs.close();
-    }
-    else
-    {
-        std::cerr << "Unable to read matrix from file \'" << fileName << "\'" << std::endl;
-        ok = false;
-    }
-    return ok;
-}
 
 
 
--- src/opencl/level1/stencil2d/Stencil2Dmain.cpp
+++ src/opencl/level1/stencil2d/Stencil2Dmain.cpp
@@ -16,6 +16,7 @@
 #include "BadCommandLine.h"
 #include "Matrix2D.h"
 #include "Matrix2D.cpp"
+#include "Matrix2DFileSupport.cpp"
 #include "InitializeMatrix2D.h"
 #include "InitializeMatrix2D.cpp"
 #include "ValidateMatrix2D.h"
@@ -46,70 +47,6 @@
 void CheckOptions( const OptionParser& opts );
 
 
-template<class T>
-std::string
-GetMatrixFileName( std::string baseName )
-{
-    // nothing to do - this should never be instantiated
-    assert( false );
-    return "";
-}
-
-template<>
-std::string
-GetMatrixFileName<float>( std::string baseName )
-{
-    return baseName + "-sp.dat";
-}
-
-template<>
-std::string
-GetMatrixFileName<double>( std::string baseName )
-{
-    return baseName + "-dp.dat";
-}
-
-
-template<class T>
-bool
-SaveMatrixToFile( const Matrix2D<T>& m, std::string fileName )
-{
-    bool ok = true;
-
-    std::ofstream ofs( fileName.c_str(), ios::out | ios::binary );
-    if( ofs.is_open() )
-    {
-        ok = m.WriteTo( ofs );
-        ofs.close();
-    }
-    else
-    {
-        std::cerr << "Unable to write matrix to file \'" << fileName << "\'" << std::endl;
-        ok = false;
-    }
-    return ok;
-}
-
-
-template<class T>
-bool
-ReadMatrixFromFile( Matrix2D<T>& m, std::string fileName )
-{
-    bool ok = true;
-
-    std::ifstream ifs( fileName.c_str(), ios::in | ios::binary );
-    if( ifs.is_open() )
-    {
-        ok = m.ReadFrom( ifs );
-        ifs.close();
-    }
-    else
-    {
-        std::cerr << "Unable to read matrix from file \'" << fileName << "\'" << std::endl;
-        ok = false;
-    }
-    return ok;
-}
 
 
 template<class T>
