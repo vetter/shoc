db10a5314316b4be497cf766f7a5d27545ad4703
Jeffrey S. Vetter <vetter@computer.org>
2020-04-19 22:24:08 -0400

    remove very old config files and directories

--- build-aux/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Bootstrapping SHOC build system."
-aclocal || exit 1
-automake --foreign --add-missing --copy || exit 1
-autoconf || exit 1
-echo "Done.  Now configure, make, and make install."
-
